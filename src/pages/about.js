1d72f158813101092fde5b4e83f28522cf22d4c9
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-12-05 15:47:36 +0000

    Added an about page.

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function About() {
+    return (
+        <div style={{ color: 'purple' }}>
+            <h1>About Keith</h1>
+            <p>Artist turned software developer.</p>
+        </div>
+    );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
 
 export default function Home() {
-  return <div>The beginning!</div>
+  return (
+    <div style={{color: 'blue'}}>
+      <h1>Keith Nimmo</h1>
+      <p>Testing stuff</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  );
 }
