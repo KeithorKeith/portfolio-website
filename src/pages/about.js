63c3e8ba8ce3cc9d56b86901302fa44d624605af
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-12-05 15:58:29 +0000

    Added a header component.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,10 @@
 import React from "react"
+import Header from "../components/header"
 
 export default function About() {
     return (
         <div style={{ color: 'purple' }}>
+            <Header />
             <h1>About Keith</h1>
             <p>Artist turned software developer.</p>
         </div>
