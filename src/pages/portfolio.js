90ac561ce654d1d9c6fa8f4445ae9072b1f024d9
Keith Nimmo <keithnimmo@hotmail.co.uk>
2021-03-03 17:02:50 +0000

    Tweaking some of the CSS so it looks nicer on larger screens.

---
+++ src/pages/portfolio.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import { Button } from "react-bootstrap"
+
+
+export default function Portfolio() {
+  return (
+    <div className="header">
+        <nav class="nav-container">
+          <a class="named-transition" href="/">
+            <span class="keith-nimmo">KN</span>
+          </a>
+          <ul class="nav-right">
+            <li class="list-item">
+              <a class="transition-link" href="#about">About</a>
+            </li>
+            <li class="list-item">
+              <a class="transition-link" href="#projects">Projects</a>
+            </li>
+            <li class="list-item">
+              <a class="transition-link" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+    </div>
+  )
+}
